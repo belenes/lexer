518e6cabde1cd90facd99db88c883b08b4e68e80
belenes <38964255+belenes@users.noreply.github.com>
2018-06-12 00:32:42 -0300

    Create lexer.py

---
+++ lexer.py
@@ -0,0 +1,34 @@
+TT = []
+
+def lexer(src):
+    tokens[]
+    src = src + " "
+    i = 0
+    start = 0
+    state = 0
+    while i<len(src):
+        c = src[i]
+        w = src[start: i+1]
+        if state == 0:
+            if c.isspace():
+                i+=1
+                state = 0
+            else:
+                start = i
+                state = 1
+        if state == 1:
+            if es_aceptada(w) or not c.isspace():
+                i+=1
+                state = 1
+            else:
+                i-=1
+                state = 2
+
+def es_aceptada(w):
+
+    candidatos = []
+
+    if len(candidatos)>0:
+        return True
+    else:
+        return False 
